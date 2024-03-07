cdde9bc02829bfc04d4b9bc710fb41ad085d8ca7
rachidcooder <cooderherou@gmail.com>
2024-03-07 11:16:30 +0000

    main url to books

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,9 +5,11 @@ import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const router = useRouter();
+
+
   useEffect(() => {
     router.push('/books');
-  })
+  }, []);
 
   return (
     <main className=" ">
